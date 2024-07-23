a95c773f53a740a5a74004dfd95e7adb4029a0af
João Carlos Matarazzo <100213241+JoaoCarlosMatarazzo@users.noreply.github.com>
2024-07-23 11:27:34 -0300

    Update data_fetcher.py

--- monitoring_dashboard/utils/data_fetcher.py
+++ monitoring_dashboard/utils/data_fetcher.py
@@ -1,1 +1,7 @@
+import pandas as pd
+
+def fetch_data():
+    # Colocar a função para buscar dados de uma API ou banco de dados
+    # Colocar exemplo fictício
+    return pd.read_csv('data/sample_data.csv')
  
